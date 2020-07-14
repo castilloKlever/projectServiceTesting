9cb07c9d36667a07302ad81623193d8db6f0859a
NesCh99 <68031777+NesCh99@users.noreply.github.com>
2020-07-14 15:59:20 -0500

    Version 0.0.1

---
+++ GraficasRiobamba/GraficasRiobamba/src/main/java/com/grafrio/GraficasRiobamba/GraficasRiobambaApplication.java
@@ -0,0 +1,13 @@
+package com.grafrio.GraficasRiobamba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraficasRiobambaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GraficasRiobambaApplication.class, args);
+	}
+
+}
---
+++ GraficasRiobamba/GraficasRiobamba/src/test/java/com/grafrio/GraficasRiobamba/GraficasRiobambaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grafrio.GraficasRiobamba;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraficasRiobambaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
