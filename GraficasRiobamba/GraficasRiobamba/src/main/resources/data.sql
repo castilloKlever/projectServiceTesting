c5e9da73628d46ca0b913075c96470403f472c2c
Fabian <Fabian@DESKTOP-B6TIBJV>
2020-12-12 16:22:05 -0500

    Conection MYSQL

--- GraficasRiobamba/GraficasRiobamba/src/main/resources/data.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO productos (nombre, categoria, descripcion,cantidad,precio,foto) VALUES
-  ('Jarro', 'Artículos Publicitarios', 'Jarros sublimados, regalos personalizados, publicitarios',2,5, 'f4cf5d8dae1e4359a2be5f32b51da45a'),
-    ('Esferos Publicitario', 'Esferos','Esferos de cualquier color con logotipo',100,0.30, '3fc9dd32d29447b192a70da78f8882e5'),
-   ('Sellos Fechadores', 'Sellos', 'Sellos personalizados automaticos',1,3, '1cde9028b1784b389212579226556c1c');
